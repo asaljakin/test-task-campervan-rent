f3cdcaf336f2cd399ff83e2420ce039bcef1dc8e
Andrew <asaljakin@gmail.com>
2024-04-17 12:17:11 +0300

    bugfix Filter & conect normalize

--- src/components/Catalog/Catalog.styled.jsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const CatalogContainer = styled.div`
-  display: flex;
-  gap: 64px;
-  justify-content: center;
-`;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.jsx';
+import 'modern-normalize';
 import './styles/variables.css';
+import './styles/globalStyles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
